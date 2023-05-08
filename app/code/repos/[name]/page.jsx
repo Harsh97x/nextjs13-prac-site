dd33d8cd5f5ee82f684f156b4d7381e1b1f861e9
Harsh97x <Rynzie@protonmail.com>
2023-05-08 17:14:30 +0530

    refactor [name]/page.jsx

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,12 +1,12 @@
-import { Suspense } from 'react';
-import Link from 'next/link';
-import Repo from '@/app/components/Repo';
-import RepoDirs from '@/app/components/RepoDirs';
+import { Suspense } from "react";
+import Link from "next/link";
+import Repo from "@/app/components/Repo";
+import RepoDirs from "@/app/components/RepoDirs";
 
 const RepoPage = ({ params: { name } }) => {
   return (
-    <div className='card'>
-      <Link href='/code/repos' className='btn btn-back'>
+    <div className="card">
+      <Link href="/code/repos" className="btn btn-back">
         Back To Repositories
       </Link>
       <Suspense fallback={<div>Loading repo...</div>}>
